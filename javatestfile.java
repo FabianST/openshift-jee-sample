8e65018470192240b380e6a5effc60cd12e35d01
FabianST <fabian.stoeckner@metafinanz.de>
2016-06-07 15:03:44 +0200

    Create javatestfile.java

---
+++ javatestfile.java
@@ -0,0 +1,17 @@
+import java.math.*
+import java.swing.*
+import java.awt.*
+
+public static void main (String args[]){
+    int [] outputarray = new int [20];
+    double summe = 0;
+    for (int i=0; i<outputarray.length; i++){
+        outputarray[i] = (int) math.random;
+        summe =+ outputarray[i];
+        System.out.println((i+1) +". "outputarray[i]);
+        if ((i+1) == outputarray.length){
+          System.out.println("Summe: "+summe);
+        }
+    }
+    
+}
